bf46ccba53238acbea395e87c9c876f480aaaa73
arthurdsfr <arthur.desfrancais@gmail.com>
2024-09-14 14:10:44 +0200

    Rutra + mint

--- contracts/MyToken.sol
+++ contracts/MyToken.sol
@@ -3,7 +3,17 @@ pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract MyToken is ERC20, ERC20Permit {
-    constructor() ERC20("MyToken", "MTK") ERC20Permit("MyToken") {}
+contract RutraToken is ERC20, ERC20Permit, Ownable {
+    constructor() ERC20("RutraToken", "RUTRA") ERC20Permit("RutraToken") Ownable(msg.sender) {
+
+    }
+
+    function mint(address _to, uint256 amount) public onlyOwner {
+    _mint(_to, amount);
 }
+
+
+}
+
